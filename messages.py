b883ed05c61f5ecc0d6998591ce92661c908ed0c
Nacho Gorostidi <nachogoro.dev@gmail.com>
2023-02-23 20:20:36 +0100

    Move to flask_restx

--- messages.py
+++ messages.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_restx import Resource, Api
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///messages.db'
 db = SQLAlchemy(app)
+api = Api(app)
 
 # define Message model
 class Message(db.Model):
@@ -14,17 +16,16 @@ class Message(db.Model):
     def __repr__(self):
         return f"Message(title='{self.title}', body='{self.body}')"
 
-
 with app.app_context():
     db.create_all()
 
-
-@app.route('/messages', methods=['POST', 'GET'])
-def handle_messages():
-    if request.method == 'POST':
+@api.route('/messages')
+class MessageResource(Resource):
+    def post(self):
+        # get authentication token from headers
         auth_token = request.headers.get('Authorization')
         if auth_token != 'secret_token':
-            return jsonify({'message': 'Unauthorized'}), 401
+            return {'message': 'Unauthorized'}, 401
 
         # get title and body from request body
         title = request.json['title']
@@ -40,19 +41,19 @@ def handle_messages():
         new_message = Message(title=title, body=body)
         db.session.add(new_message)
         db.session.commit()
-        return jsonify({'message': 'Message saved successfully.'})
+        return {'message': 'Message saved successfully.'}, 201
 
-    elif request.method == 'GET':
+    def get(self):
         # get title from request parameter
         title = request.args.get('title')
         # retrieve message from database
         message = Message.query.filter_by(title=title).first()
         if message:
             # return message as JSON object
-            return jsonify({'title': message.title, 'body': message.body})
+            return {'title': message.title, 'body': message.body}, 200
         else:
-            return jsonify({'message': 'Message not found.'}), 404
-
+            return {'message': 'Message not found.'}, 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
