7d22267fb1a0d0bdfb7bb8a25931e8760f53dd0a
NachoGoro <nachogoro.dev@gmail.com>
2023-03-21 20:41:47 +0100

    Add HTTPS support
    Use on-the-fly HTTPS certificates

--- messages.py
+++ messages.py
@@ -56,5 +56,5 @@ class MessageResource(Resource):
 api.add_resource(MessageResource, '/messages')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, ssl_context='adhoc', debug=True)
 
