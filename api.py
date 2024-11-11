e5d059e3e2f433df5cc872b781546fada552030e
NachoGoro <nachogoro.dev@gmail.com>
2024-11-11 20:35:40 +0100

    Add support for HTTPS

--- api.py
+++ api.py
@@ -282,4 +282,4 @@ class BookStream(Resource):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, ssl_context='adhoc')
